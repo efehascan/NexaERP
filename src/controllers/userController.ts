43452921b9fe2053727fe031019712ba58cfd270
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 10:45:57 +0300

    userController

---
+++ src/controllers/userController.ts
@@ -0,0 +1,31 @@
+﻿import { Request, Response } from "express";
+import * as userService from "../services/userService";
+import { UserRole } from "../enums/UserRole";
+
+export async function register(req: Request, res: Response) {
+    const { username, password, role, companyId } = req.body;
+
+    try{
+        const user = await userService.createUser(
+            username,
+            password,
+            role || UserRole.USER,
+            companyId,
+        );
+
+        res.status(201).json({
+            message: "Kullanıcı oluşturuldu",
+            user: {
+                id: user.id,
+                username: user.username,
+                role: user.role,
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: "Kullanıcı oluşturulamadı",
+            error: (err as Error).message,
+        })
+    }
+}
+
