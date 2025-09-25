a2ce322780d3231ebebfbead3f5fe9b477359874
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 10:48:21 +0300

    roleMiddleware

---
+++ src/middleware/roleMiddleware.ts
@@ -0,0 +1,12 @@
+﻿import { Request, Response, NextFunction } from "express";
+import { UserRole } from "../enums/UserRole";
+
+export function authorizeRoles(...allowedRoles: UserRole[]) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const user = (req as any).user;
+        if (!user || !allowedRoles.includes(user.role)) {
+            return res.status(403).json({ message: "Yetkisiz erişim" });
+        }
+        next();
+    };
+}
