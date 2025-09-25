86e2099c8318fe2d5d6fb9dc751958ce05aac56f
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 10:47:17 +0300

    authService

---
+++ src/controllers/authController.ts
@@ -0,0 +1,13 @@
+﻿import { Request, Response } from "express";
+import * as authService from "../services/authService";
+
+export async function login(req: Request, res: Response) {
+    const { username, password } = req.body;
+
+    try {
+        const token = await authService.login(username, password);
+        res.json({ token });
+    } catch (err) {
+        res.status(401).json({ message: (err as Error).message });
+    }
+}
