dd845be6633c619ee69b0654f6fd286a33977d05
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 09:45:28 +0300

    UserServices

---
+++ src/services/userService.ts
@@ -0,0 +1,26 @@
+﻿import { AppDataSource } from "../config/data-source";
+import { User } from "../entity/User";
+import { hashPassword } from "../utils/bcryptUtils";
+import { UserRole } from "../enums/UserRole";
+
+const userRepo = AppDataSource.getRepository(User);
+
+export async function createUser(
+    username : string,
+    password: string,
+    role: UserRole,
+    companyId: number
+) {
+    const hashed = await hashPassword(password);
+    const user = userRepo.create({
+        username,
+        password: hashed,
+        role,
+        companyId,
+    });
+    return await userRepo.save(user);
+}
+
+export async function findUserByUsername(username: string){
+    return await userRepo.findOne({where: {username}});
+}
