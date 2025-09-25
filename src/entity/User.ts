205e36c4fc5f96ade4f660dc54a907f30348dfd2
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 09:37:59 +0300

    User typeorm

---
+++ src/entity/User.ts
@@ -0,0 +1,27 @@
+﻿import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { UserRole } from "../enums/UserRole";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ unique: true })
+    username!: string;
+
+    @Column()
+    password!: string;
+
+    @Column({
+        type: "enum",
+        enum: UserRole,
+        default: UserRole.USER
+    })
+    role!: UserRole;
+
+    @Column()
+    companyId!: number;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt!: Date;
+}
