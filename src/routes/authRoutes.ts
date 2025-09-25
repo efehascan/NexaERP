6528df5b2c26d95f8db87660db29d61b6bfa36f3
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 10:49:41 +0300

    Routes

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,8 @@
+﻿import { Router } from "express";
+import { login } from "../controllers/authController";
+
+const router = Router();
+
+router.post("/login", login);
+
+export default router;
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+﻿import { Router } from "express";
+import { register } from "../controllers/userController";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { authorizeRoles } from "../middleware/roleMiddleware";
+import { UserRole } from "../enums/UserRole";
+
+const router = Router();
+
+router.post("/register", authMiddleware, authorizeRoles(UserRole.ADMIN), register);
+
+export default router;
