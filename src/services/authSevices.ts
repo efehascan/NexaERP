2a2dd1b92ab18d080fba84322ca536cdb775070a
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 09:52:31 +0300

    Auth Services

---
+++ src/services/authSevices.ts
@@ -0,0 +1,22 @@
+﻿import { comparePassword } from "../utils/bcryptUtils";
+import { generateToken } from "../utils/jwtUtils";
+import { findUserByUsername } from "./userService";
+
+export async function login(username: string, password: string) {
+    const user = await findUserByUsername(username);
+    if (!user) {
+        throw new Error("Invalid username or password");
+    }
+
+    const isValid = await comparePassword(password, user.password);
+    if (!isValid) {
+        throw new Error("Invalid password");
+    }
+
+    return generateToken({
+        userId: user.id,
+       companyId: user.companyId,
+       role: user.role,
+    });
+
+}
