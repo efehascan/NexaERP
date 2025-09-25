5bee85cb8c026d5eb1992bded4e7ec0c7d22f086
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-09-25 10:52:39 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,20 @@ import { AppDataSource } from "./config/data-source";
 import app from "./app";
 import { env } from "./config/env";
 
-AppDataSource.initialize()
-    .then(() => {
+async function startServer() {
+    try {
+        await AppDataSource.initialize();
         console.log("Database connected");
 
         app.listen(env.PORT, () => {
             console.log(
-                ` Server running at http://localhost:${env.PORT} in ${env.NODE_ENV} mode`
+                `Server running at http://localhost:${env.PORT} in ${env.NODE_ENV} mode`
             );
         });
-    })
-    .catch((err) => {
+    } catch (err) {
         console.error("Database connection failed:", err);
-    });
+        process.exit(1); // Hata olursa process'i kapat
+    }
+}
+
+startServer();
